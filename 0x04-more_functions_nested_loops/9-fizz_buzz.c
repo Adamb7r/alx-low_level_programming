9b219baa5b7c1459f38d8c0ab4c7775a4d26638d
Adamb7r <bahrcr7@gmai.com>
2023-08-17 10:29:28 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,28 +14,32 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0 && num % 5 != 0)
+		if (num % 3 == 0 && !(num % 5 == 0))
 		{
-			printf(" Fizz");
+			printf("Fizz");
 		}
-		else if (num % 5 == 0 && num % 3 != 0)
+		else if (num % 5 == 0 && !(num % 3 == 0))
 		{
-			printf(" Buzz");
+			printf("Buzz");
 		}
 		else if (num % 3 == 0 && num % 5 == 0)
 		{
-			printf(" FizzBuzz");
-		}
-		else if (num == 1)
-		{
-			printf("%d", i);
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf(" %d", i);
+			printf("%d", num);
+		}
+		if (num != 100)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
+
 		}
 	}
-			printf("\n");
 
 	return (0);
 }
