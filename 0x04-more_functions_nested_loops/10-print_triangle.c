9a8cb51a19ec049305c70706f36f48cf2fff078e
Adamb7r <bahrcr7@gmai.com>
2023-08-17 10:39:49 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -20,14 +20,12 @@ void print_triangle(int size)
 		{
 			for (j = 1; j < size; j++)
 			{
-				_putchar(' ');
-			}
-
-				for (j = 1; j <= i; j++)
-				{
+				if ((i + j) <= size)
+					_putchar(' ');
+				else
 					_putchar('#');
-				}
-				_putchar('\n');
+			}
+			_putchar('\n');
 		}
 	}
 }
