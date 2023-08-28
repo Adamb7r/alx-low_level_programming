a00e0e4c665d37be54c0bde78d6ed2a3006b9abb
Adamb7r <bahrcr7@gmai.com>
2023-08-28 10:52:37 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Entry point
+ * @a: 2d array of int tybes
+ * @size: size of array
+ * Return: Always 0 (Success)
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int num1, num2, y;
+
+	num1 = 0;
+	num2 = 0;
+
+	for (y = 0; y < size; y++)
+	{
+		num1 = num1 + a[y * size + y];
+	}
+
+	for (y = size - 1; y >= 0; y--)
+	{
+		num2 += a[y * size + (size - y - 1)];
+	}
+
+	printf("%d, %d\n", num1, num2);
+}
